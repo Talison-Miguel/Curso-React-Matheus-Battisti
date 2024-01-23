5138ecf0b97bc901201699090fb59d056b26f737
Tálison Miguel <talisonmiguel84@gmail.com>
2024-01-23 14:03:33 -0300

    add: page home

--- 2-projeto-react/src/App.jsx
+++ 2-projeto-react/src/App.jsx
@@ -1,9 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import { Home } from './components/pages/Home'
 import { Projects } from './components/pages/Projects'
 import { Company } from './components/pages/Company'
-import { Contatc } from './components/pages/Contatc'
+import { Contact } from './components/pages/Contact'
 import { NewProject } from './components/pages/NewProject'
 
 import { Container } from './components/layout/Container'
@@ -18,7 +18,7 @@ function App() {
         <Routes>
             <Route exact path="/" element={<Home />}/>
             <Route exact path="/projects" element={<Projects />}/>
-            <Route exact path="/contatc" element={<Contatc />}/>
+            <Route exact path="/contact" element={<Contact />}/>
             <Route exact path="/company" element={<Company />}/>
             <Route exact path="/newproject" element={<NewProject />}/>
         </Routes>
